dd8dfcfda198feef5069b5fd28e6c660789825dd
Jeff <Jffrypwll@googlemail.com>
2016-06-07 12:40:47 +0100

    update

--- www/dashboard.php
+++ www/dashboard.php
@@ -1,11 +1,53 @@
 <?php
 
+create_graph("calls-gw-tok-halfday-wall.png",   "-12h",         "Tokyo calls last 12 hours",             "200", "1100");
+
 echo "<html><head>";
 echo "<meta http-equiv=\"refresh\" content=\"30\">";
 echo "</head><body bgcolor='#080808'>";
 
+echo "<img src='calls-gw-tok-halfday-wall.png'>";
 
 echo "</body></html>";
 exit;
+
+function create_graph($output, $start, $title, $height, $width) {
+
+  $options = array(
+    "--slope-mode",
+    "--start", $start,
+    "--title=$title",
+    "--vertical-label=Calls",
+    "--lower=0",
+    "--height=$height",
+    "--width=$width",
+    "-cBACK#161616",
+    "-cCANVAS#1e1e1e",
+    "-cSHADEA#000000",
+    "-cSHADEB#000000",
+    "-cFONT#c7c7c7",
+    "-cGRID#888800",
+    "-cMGRID#ffffff",
+    "-nTITLE:10",
+    "-nAXIS:12",
+    "-nUNIT:10",
+    "-y 1:5",
+    "-cFRAME#ffffff",
+    "-cARROW#000000",
+    "DEF:callmax=/usr/local/scripts/git/jcall2/data/jcall-gw-tok.rrd:callstot:MAX",
+    "CDEF:transcalldatamax=callmax,1,*",
+    "AREA:transcalldatamax#a0b84240",
+    "LINE4:transcalldatamax#a0b842:Calls",
+    "COMMENT:\\n",
+#    "GPRINT:transcalldatamax:LAST:Calls Now %6.2lf",
+    "GPRINT:transcalldatamax:MAX:Calls Max %6.2lf"
+  );
+
+ $ret = rrd_graph($output, $options, count($options));
+
+  if (! $ret) {
+    echo "<b>Graph error: </b>".rrd_error()."\n";
+  }
+}
 
 ?>
