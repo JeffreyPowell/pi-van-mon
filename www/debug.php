e1dcb0bb7b4e13b858f864fb7566168eac017092
Jeffrey Powell <jffrypwll@googlemail.com>
2016-06-21 08:05:54 +0100

    debug

--- www/debug.php
+++ www/debug.php
@@ -9,11 +9,11 @@ echo "<html><head>";
 echo "<meta http-equiv=\"refresh\" content=\"10\">";
 echo "</head><body bgcolor='#FAFAFA'>";
 
-exec("date")
+exec("date");
 
-exec("tail /var/log/messages")
+exec("tail /var/log/messages");
 
-exec("ll /var/log/")
+exec("ll /var/log/");
 
 echo "</body></html>";
 
