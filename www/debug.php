423c5f735c76a5efe2f0151d53e0ab9ebd4dbd23
Jeff <Jffrypwll@googlemail.com>
2016-06-23 07:34:55 +0100

    .

--- www/debug.php
+++ www/debug.php
@@ -1,45 +1,34 @@
 <?php
-
-#create_graph("calls-gw-tok-halfday-wall.png",   "-12h",         "Tokyo calls last 12 hours",             "200", "1100");
+$height="100";
+$width="200";
 
 echo "<html><head>";
-echo "<meta http-equiv='refresh' content='30'>";
-echo "<style>";
-echo "body {background-color:white; border:1px solid red; padding:5px; font-family:sans-serif;}";
-#echo "#header {padding:5px; font-family:sans-serif; font-size:22px; text-align:center; color:white; background-color:black;}";
-#echo "#nav {border:1px solid red; padding:5px; font-family:sans-serif; font-size:12px; text-align:center; display: block; background-color:yellow;}";
-echo "#p01 {color: blue;}";
-echo "</style>";
+echo "<meta http-equiv=\"refresh\" content=\"10\">";
+echo "<link rel='stylesheet' href='debug.css'>";
 echo "</head><body>";
 
-echo "<div id='device'>";
+echo "<h1>";
+echo shell_exec("date");
+echo "</h1>";
 
-echo "<div>";
-echo "This is page heading";
+echo "<br>";
+echo "<h1>messages</h1>";
+echo "<div id='code'>";
+echo shell_exec("tail -n 20 /var/log/messages");
 echo "</div>";
 
+echo "<br>";
+echo "<h1>error.log</h1>";
+echo "<div id='code'>";
+echo shell_exec("tail -n 20 /var/log/apache2/error.log");
 echo "</div>";
 
-echo "<div id='nav'>";
-echo "<table id='nav'><tr>";
-echo "<td><a href='#'>Home</a></td>";
-echo "<td><a href='#'>About Us</a></td>";
-echo "<td><a href='#'>Contact Us</a></td>";
-echo "</tr></table></div>";
-
-#echo "<img src='calls-gw-tok-halfday-wall.png'>";
-echo "<p id='p01'>I am different</p>";
-echo "";
-echo "<img src='images/120x240.png'>";
-echo "<img src='images/240x120.png'>";
-
-echo "";
-echo "";
-echo "";
-echo "";
-echo "";
-
+echo "<br>";
+echo "<h1>error.log</h1>";
+echo "<div id='code'>";
+echo shell_exec("tail -n 20 /var/log/apache2/access.log");
+echo "</div>";
 echo "</body></html>";
-exit;
 
-?>
+exit;
+</php>
