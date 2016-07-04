08347f3237b0ff9797563bd2363f9407940db2da
pi@rpi-van <jffrypwll@googlemail.com>
2016-07-04 02:19:36 +0100

    .

--- poll-adci2c.py
+++ poll-adci2c.py
@@ -6,19 +6,19 @@ import datetime
 import os
 import yaml
 
-config = yaml.safe_load(open("config.yaml"))
+config = yaml.safe_load(open("/home/pi/bin/van/config.yaml"))
 
-print( config )
+#print( config )
 
 for device in config['devices']['adci2c']:
-    print( device )
+    #print( device )
 
     adda = int(config['devices']['adci2c'][device]['adda'])
     addb = int(config['devices']['adci2c'][device]['addb'])
 
     rate = int(config['devices']['adci2c'][device]['rate'])
 
-    print( adda, addb, rate )
+    #print( adda, addb, rate )
 
     
 
@@ -26,29 +26,26 @@ for device in config['devices']['adci2c']:
 
     for pin in config['devices']['adci2c'][device]['pins']:
 
-      print( pin )
+      #print( pin )
 
       t = datetime.datetime.now().strftime('%s')
 
       factor = float(config['devices']['adci2c'][device]['pins'][pin]['factor'])
       offset = float(config['devices']['adci2c'][device]['pins'][pin]['offset'])
 
-      print( factor, offset )
+      #print( factor, offset )
 
       rawdata = adc.readRaw( pin )
       data = str( ( rawdata * factor ) - offset )
 
-      print( rawdata, data )
+      #print( rawdata, data )
 
       filename = '/home/pi/bin/van/data/adci2c-'+str(device)+'-'+str(pin)+'.rrd'      
 
 
       if( not os.path.exists( filename ) ):
-        print ( os.path.exists( filename ))
+        #print ( os.path.exists( filename ))
         os.system('/usr/bin/rrdtool create '+filename+' --step 60 --start now DS:data:GAUGE:120:U:U RRA:AVERAGE:0.5:1:1400 RRA:AVERAGE:0.5:5:8640 RRA:AVERAGE:0.5:60:8760')
 
       os.system('/usr/bin/rrdtool update '+filename+" "+str(t)+':'+data)
 
-      #os.system('/usr/bin/rrdtool update /usr/local/scripts/git/pi-adc-mon/data/adc-volts.rrd `date +"%s"`:$V1:$V2:$V3:$V4:$V5:$V6:$V7:$V8')
-
-      #os.system('/usr/bin/rrdtool update /usr/local/scripts/git/pi-adc-mon/data/adc-volts.rrd '+str(t)+':'+str(s1))
--- www/devices.php
+++ www/devices.php
@@ -1,5 +1,21 @@
 <?php
 
+$config = parse_ini_file('/home/pi/bin/van/www/config.ini', true);
+
+print_r( $config );
+
+foreach( $config['display']['device'] as $device )
+{
+  print_r( $device );
+  print_r( "===\n" );
+  print_r( $config['display']['device'][device]['type'] );
+  print_r( "---\n" );
+}
+
+
+
+
+
 echo "<html><head>";
 echo "<meta http-equiv=\"refresh\" content=\"30\">";
 echo "</head><body bgcolor='#808080'>";
