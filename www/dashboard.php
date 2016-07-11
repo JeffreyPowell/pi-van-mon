d312d81b2118386d7c8f98513e583ae63a114ba4
Jeffrey Powell <jffrypwll@googlemail.com>
2016-07-11 23:52:04 +0100

    .

--- www/dashboard.php
+++ www/dashboard.php
@@ -153,7 +153,7 @@ function read_last_value($rrd_filename) {
   if (! $ret) {
     echo "<b>Read error: </b>".rrd_error()."\n";
   }
-  $data = $ret['data'];
+  $data = $ret['data'][0];
   print_r( "X" );
   print_r( $ret );
   print_r( $data );
