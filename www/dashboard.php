1b6f2d7f79dad0785df962de29b0254052a7c70d
JeffreyPowell <jffrypwll@googlemail.com>
2016-07-12 13:04:40 +0100

    .

--- www/dashboard.php
+++ www/dashboard.php
@@ -90,8 +90,19 @@ echo "<p style='border: 0px solid red; font-family:sans-serif; font-size:9px; te
 
 echo "</div>";
 
-echo "<div style='display: inline; background-color:#242424; background-image: url(images/120x240.png); height: 120px; width: 240px; border: 1px solid yellow;'>";
-echo "Athree";
+$device_id      = 14;
+$device_type    = (string) $config['devices']['type'][$device_id];
+$device_ref     = (string) $config['devices']['ref'][$device_id];
+$device_pin_num = (string) $config['devices']['pin'][$device_id];
+$device_name    = (string) $config['devices']['name'][$device_id];
+$device_units   = (string) $config['devices']['units'][$device_id];
+$rrd_name       = $device_type.'-'.$device_ref.'-'.$device_pin_num;
+$rrd_filename   = '/home/pi/bin/van/data/'.$rrd_name.'.rrd';
+$last_value     = read_last_value($rrd_filename);
+echo "<div style='display: inline; background-color:#242424; background-image: url(images/none.png); height: 120px; width: 240px; border: 5px solid black;'>";
+echo "<p style='border: 0px solid red; font-family:sans-serif; font-size:12px; text-align:center; color:white;'>$device_name</p>";
+echo "<p style='border: 0px solid red; font-family:sans-serif; font-size:9px; text-align:center; color:white;'>$last_value</p>";echo "<p style='border: 0px solid red; font-family:sans-serif; font-size:9px; text-align:center; color:white;'>$device_units</p>";
+
 echo "</div>";
 
 echo "</td>";
