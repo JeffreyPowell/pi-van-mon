cfedfa1a875538db24146290543d349be972c25a
JeffreyPowell <jffrypwll@googlemail.com>
2016-07-12 09:57:03 +0100

    .

--- hooks/git-pull.sh
+++ hooks/git-pull.sh
@@ -4,6 +4,6 @@ cd /home/pi/bin/van
 
 git pull | mail -s "Van Git Pull1" jffrypwll@googlemail.com
 
-$result={ git pull }
+$result=$( git pull )
 
 echo $result | mail -s "Van Git Pull2" jffrypwll@googlemail.com
