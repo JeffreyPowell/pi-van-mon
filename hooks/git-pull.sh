59caaf569f6f92801037806b5336ffeecd65760b
JeffreyPowell <jffrypwll@googlemail.com>
2016-07-12 10:30:03 +0100

    .

--- hooks/git-pull.sh
+++ hooks/git-pull.sh
@@ -6,7 +6,7 @@ git pull | mail -s "Van Git Pull1" jffrypwll@googlemail.com
 
 result=$( git pull )
 
-if [ $result = "Already up-to-date." ]; then
+if [ "$result" = "Already up-to-date." ]; then
   echo ">$result<" | mail -s "Van Git Pull2" jffrypwll@googlemail.com
 else
   echo ">$result<" | mail -s "Van Git Pull3" jffrypwll@googlemail.com
