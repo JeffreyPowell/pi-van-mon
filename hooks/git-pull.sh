c33c5166a603edeaf7f4a53601747883e2110254
JeffreyPowell <jffrypwll@googlemail.com>
2016-07-12 10:41:38 +0100

    .

--- hooks/git-pull.sh
+++ hooks/git-pull.sh
@@ -6,8 +6,9 @@ cd /home/pi/bin/van
 
 result=$( git pull )
 
-if [ "$result" = " Already up-to-date." ]; then
+if [ "$result" = "Already up-to-date." ]; then
   echo "[$result]" | mail -s "Van Git Pull - no change" jffrypwll@googlemail.com
 else
   echo "[$result]" | mail -s "Van Git Pull - something changed" jffrypwll@googlemail.com
 fi
+#
