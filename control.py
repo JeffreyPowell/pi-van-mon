9e274aeee92a1385a79d65473be546612db651af
Jeffrey Powell <jffrypwll@googlemail.com>
2016-07-16 23:11:23 +0100

    Sat Jul 16 23:11:23 BST 2016

--- control.py
+++ control.py
@@ -6,28 +6,28 @@ try:
 except RuntimeError:
     print("Error importing RPi.GPIO!  This is probably because you need superuser privileges.  You can achieve this by using 'sudo' to run your script")
 
-GPIO.setmode(GPIO.BCM)
+GPIO.setmode(GPIO.BOARD)
 
 GPIO.setwarnings(False)
 
-chan_list = [6,13,19,26]
+chan_list = [31,33,35,37]
 
 GPIO.setup(chan_list, GPIO.OUT, initial=GPIO.HIGH)
 
 while True :
 
-    GPIO.output(06, not GPIO.input(06))
+    GPIO.output(31, not GPIO.input(31))
 
     time.sleep(1)
 
-    GPIO.output(13, not GPIO.input(13))
+    GPIO.output(33, not GPIO.input(33))
 
     time.sleep(1)
 
-    GPIO.output(19, not GPIO.input(19))
+    GPIO.output(35, not GPIO.input(35))
 
     time.sleep(1)
 
-    GPIO.output(26, not GPIO.input(26))
+    GPIO.output(37, not GPIO.input(37))
 
     time.sleep(1)
