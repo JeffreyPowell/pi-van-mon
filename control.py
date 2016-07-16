2ab42078e35d69f7c5558dbad35b7aa9124d99e9
Jeffrey Powell <jffrypwll@googlemail.com>
2016-07-16 23:33:55 +0100

    Sat Jul 16 23:33:55 BST 2016

--- control.py
+++ control.py
@@ -22,48 +22,48 @@ while True :
 
     GPIO.output(31, 1)
 
-    time.sleep(1)
+    time.sleep(0.1)
 
     GPIO.output(31, 0)
 
-    time.sleep(1)
+    time.sleep(0.1)
 
     GPIO.output(31, 1)
 
-    time.sleep(1)
+    time.sleep(0.1)
 
     GPIO.output(31, 0)
 
-    time.sleep(1)
-    
+    time.sleep(0.1)
+
     GPIO.output(31, not GPIO.input(31))
 
-    time.sleep(1)
+    time.sleep(0.1)
 
     GPIO.output(33, not GPIO.input(33))
 
-    time.sleep(1)
+    time.sleep(0.1)
 
     GPIO.output(35, not GPIO.input(35))
 
-    time.sleep(1)
+    time.sleep(0.1)
 
     GPIO.output(37, not GPIO.input(37))
 
-    time.sleep(1)
+    time.sleep(0.1)
 
     GPIO.output(32, not GPIO.input(32))
 
-    time.sleep(1)
+    time.sleep(0.1)
 
     GPIO.output(36, not GPIO.input(36))
 
-    time.sleep(1)
+    time.sleep(0.1)
 
     GPIO.output(38, not GPIO.input(38))
 
-    time.sleep(1)
+    time.sleep(0.1)
 
     GPIO.output(40, not GPIO.input(40))
 
-    time.sleep(1)
+    time.sleep(0.1)
