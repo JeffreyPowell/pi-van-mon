46ac976b316a4bc0b70126f07556495ca9718fcd
Jeffrey Powell <jffrypwll@googlemail.com>
2016-07-16 23:18:27 +0100

    Sat Jul 16 23:18:27 BST 2016

--- control.py
+++ control.py
@@ -14,6 +14,10 @@ chan_list = [31,33,35,37]
 
 GPIO.setup(chan_list, GPIO.OUT, initial=GPIO.LOW)
 
+chan_list = [32,36,38,40]
+
+GPIO.setup(chan_list, GPIO.OUT, initial=GPIO.LOW)
+
 while True :
 
     GPIO.output(31, not GPIO.input(31))
@@ -29,5 +33,21 @@ while True :
     time.sleep(1)
 
     GPIO.output(37, not GPIO.input(37))
+
+    time.sleep(1)
+
+    GPIO.output(32, not GPIO.input(32))
+
+    time.sleep(1)
+
+    GPIO.output(36, not GPIO.input(36))
+
+    time.sleep(1)
+
+    GPIO.output(38, not GPIO.input(38))
+
+    time.sleep(1)
+
+    GPIO.output(40, not GPIO.input(40))
 
     time.sleep(1)
