bcdebb547eaa41e439c715860a66ac35fdfdafd1
Jeffrey Powell <jffrypwll@googlemail.com>
2016-07-17 11:08:19 +0100

    Sun Jul 17 11:08:19 BST 2016

--- control.py
+++ control.py
@@ -7,7 +7,8 @@ GPIO.setmode(GPIO.BOARD)
 
 GPIO.setmode(GPIO.BOARD)
 
-chan_list = [31,33,35,37,32,36,38,40]
+#chan_list = [31,33,35,37,32,36,38,40]
+chan_list = [31,35,37,32,36,40]
 
 GPIO.setup(chan_list, GPIO.OUT)
 
