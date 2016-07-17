2b4556af715d18edac772995772419885b09e765
Jeffrey Powell <jffrypwll@googlemail.com>
2016-07-17 14:51:23 +0100

    Sun Jul 17 14:51:23 BST 2016

---
+++ test.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+echo "40" > /sys/class/gpio/export
+echo "out" > /sys/class/gpio/gpio40/direction
+
+echo 0 > /sys/class/gpio/gpio40/value
+sleep 1
+echo 1 > /sys/class/gpio/gpio40/value
+sleep 1
+echo 0 > /sys/class/gpio/gpio40/value
+sleep 1
+echo 1 > /sys/class/gpio/gpio40/value
+sleep 1
+echo 0 > /sys/class/gpio/gpio40/value
+sleep 1
+echo 1 > /sys/class/gpio/gpio40/value
+sleep 1
+echo 0 > /sys/class/gpio/gpio40/value
+sleep 1
+echo 1 > /sys/class/gpio/gpio40/value
+sleep 1
+echo 0 > /sys/class/gpio/gpio40/value
+sleep 1
+echo 1 > /sys/class/gpio/gpio40/value
+sleep 1 
