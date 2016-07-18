a763be6b9c046d9c2f18376e886f422d43024684
JeffreyPowell <jffrypwll@googlemail.com>
2016-07-18 10:49:52 +0100

    Mon 18 Jul 2016 10:49:52 BST

--- hooks/git-pull.sh
+++ hooks/git-pull.sh
@@ -4,9 +4,12 @@ cd /home/pi/bin/van
 
 result=$( git pull 2>&1 )
 
-if [ "$result" != "Already up-to-date." ]; then
+if [ "$result" = "Already up-to-date." ]; then
+  #echo "$result" | mail -s "Van Git Pull - NO CHANGE" jffrypwll@googlemail.com
+  sleep 1
+elif [[ $result == *"Couldn't resolve host"* ]]
+  sleep 1
+else
   result+=$( echo && echo && git log -p -n 1 2>&1 )
   echo "$result" | mail -s "Van Git Pull - UPDATED" jffrypwll@googlemail.com
-#else
-  #echo "$result" | mail -s "Van Git Pull - NO CHANGE" jffrypwll@googlemail.com
 fi
