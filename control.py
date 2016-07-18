333417bbe6eb32d0e3410330a213eb2e312c623b
Jeffrey Powell <jffrypwll@googlemail.com>
2016-07-18 03:01:17 +0100

    Mon Jul 18 03:01:17 BST 2016

--- control.py
+++ control.py
@@ -10,73 +10,28 @@ print GPIO.RPI_REVISION
 #GPIO.setmode(GPIO.BOARD)
 GPIO.setmode(GPIO.BCM)
 
-#chan_list = [31,33,35,37,32,36,38,40]
-chan_list = [6]
+chan_list = [31,33,35,37,32,36,38,40]
+#chan_list = [6]
 
 time.sleep(1)
 
-GPIO.setup(chan_list, GPIO.OUT)
+for pin in chan_list:
+    print pin
 
-n = 10
+    GPIO.setup(pin, GPIO.OUT)
 
-while n>0 :
-    n = n - 1
-    print n
+    n = 10
 
-    GPIO.output(6, GPIO.HIGH)
+    while n>0 :
+        n = n - 1
+        print n
 
-    time.sleep(0.1)
+        GPIO.output(pin, GPIO.HIGH)
 
-    GPIO.output(6, GPIO.LOW)
+        time.sleep(0.1)
 
-    time.sleep(0.1)
-'''
-    GPIO.output(40, True)
+        GPIO.output(pin, GPIO.LOW)
 
-    time.sleep(0.1)
-
-    GPIO.output(40, False)
-
-    time.sleep(0.1)
-
-    GPIO.output(31, 1)
-
-    time.sleep(0.1)
-
-    GPIO.output(31, 0)
-
-    time.sleep(0.1)
-
-    GPIO.output(31, not GPIO.input(31))
-
-    time.sleep(0.1)
-
-    GPIO.output(33, not GPIO.input(33))
-
-    time.sleep(0.1)
-
-    GPIO.output(35, not GPIO.input(35))
-
-    time.sleep(0.1)
-
-    GPIO.output(37, not GPIO.input(37))
-
-    time.sleep(0.1)
-
-    GPIO.output(32, not GPIO.input(32))
-
-    time.sleep(0.1)
-
-    GPIO.output(36, not GPIO.input(36))
-
-    time.sleep(0.1)
-
-    GPIO.output(38, not GPIO.input(38))
-
-    time.sleep(0.1)
-
-    GPIO.output(40, not GPIO.input(40))
-
-    time.sleep(0.1)
-'''
+        time.sleep(0.1)
+        
 GPIO.cleanup()
