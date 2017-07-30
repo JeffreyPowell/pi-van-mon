50e3712c38ef6c5371065b670a7d8cc05aa6fd42
Jeff <jffrypwll@googlemail.com>
2017-07-30 22:14:22 +0100

    .

--- scripts/pi-van-mon-update.sh
+++ scripts/pi-van-mon-update.sh
@@ -12,4 +12,6 @@ elif [[ $result == *"Couldn't resolve host"* ]]
 else
   result+=$( echo && echo && git log -p -n 1 2>&1 )
   echo "$result" | mail -s "Van Git Pull - UPDATED" jffrypwll@googlemail.com
+
+  cp -r "/home/pi/code/pi-van-mon/www" "/var/www/pi-van-mon"
 fi
