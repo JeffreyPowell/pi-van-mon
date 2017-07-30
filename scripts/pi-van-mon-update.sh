e1f7f6a89321d5e64165487931157eb29e3733ab
Jeff <jffrypwll@googlemail.com>
2017-07-31 00:20:32 +0100

    .

--- scripts/pi-van-mon-update.sh
+++ scripts/pi-van-mon-update.sh
@@ -5,13 +5,13 @@ cd /home/pi/code/pi-van-mon
 result=$( git pull 2>&1 )
 
 if [ "$result" = "Already up-to-date." ]; then
-  echo "$result" | mail -s "Van Git Pull - NO CHANGE" jffrypwll@googlemail.com
+  #echo "$result" | mail -s "Van Git Pull - NO CHANGE" jffrypwll@googlemail.com
   sleep 1
 elif [[ $result == *"Couldn't resolve host"* ]]
   sleep 1
 else
   result+=$( echo && echo && git log -p -n 1 2>&1 )
-  echo "$result" | mail -s "Van Git Pull - UPDATED" jffrypwll@googlemail.com
+  #echo "$result" | mail -s "Van Git Pull - UPDATED" jffrypwll@googlemail.com
 
   cp -r "/home/pi/code/pi-van-mon/www" "/var/www/pi-van-mon"
 fi
