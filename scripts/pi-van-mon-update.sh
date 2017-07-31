9708e2666e72a568adc7cfcf89f2aa7fa9280e7a
Jeff <jffrypwll@googlemail.com>
2017-08-01 00:33:46 +0100

    .

--- scripts/pi-van-mon-update.sh
+++ scripts/pi-van-mon-update.sh
@@ -1,16 +1,19 @@
 #!/bin/bash
 
-cd /home/pi/code/pi-van-mon
+cd /home/pi/bin/pi-van-mon
 
 result=$( git pull 2>&1 )
 
 if [ "$result" = "Already up-to-date." ]; then
+  echo $result
   #echo "$result" | mail -s "Van Git Pull - NO CHANGE" jffrypwll@googlemail.com
   sleep 1
-elif [[ $result == *"Couldn't resolve host"* ]]
+elif [[ $result == *"Couldn't resolve host"* ]]; then
+  echo $result
   sleep 1
 else
   result+=$( echo && echo && git log -p -n 1 2>&1 )
+  echo $result
   #echo "$result" | mail -s "Van Git Pull - UPDATED" jffrypwll@googlemail.com
 
   cp -r "/home/pi/bin/pi-van-mon/www" "/var/www/pi-van-mon"
