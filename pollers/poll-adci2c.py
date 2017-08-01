5966b57e72c6255f918db9290eb002616ef3c15f
Jeff <jffrypwll@googlemail.com>
2017-08-01 20:35:55 +0100

    .

--- pollers/poll-adci2c.py
+++ pollers/poll-adci2c.py
@@ -22,32 +22,32 @@ for device in config['devices']['adci2c']:
 
 
     #try:
-        adc = ADCPi( adda, addb, rate)
+    adc = ADCPi( adda, addb, rate)
 
-        for pin in config['devices']['adci2c'][device]['pins']:
+    for pin in config['devices']['adci2c'][device]['pins']:
 
-          print( pin )
+      print( pin )
 
-          t = datetime.datetime.now().strftime('%s')
+      t = datetime.datetime.now().strftime('%s')
 
-          factor = float(config['devices']['adci2c'][device]['pins'][pin]['factor'])
-          offset = float(config['devices']['adci2c'][device]['pins'][pin]['offset'])
+      factor = float(config['devices']['adci2c'][device]['pins'][pin]['factor'])
+      offset = float(config['devices']['adci2c'][device]['pins'][pin]['offset'])
 
-          print( factor, offset )
+      print( factor, offset )
 
-          rawdata = adc.readVoltage( pin )
-          data = str( ( rawdata + offset ) * factor )
+      rawdata = adc.readVoltage( pin )
+      data = str( ( rawdata + offset ) * factor )
 
-          print( device, pin, rawdata, data )
+      print( device, pin, rawdata, data )
 
-          filename = '/home/pi/bin/pi-van-mon/data/adci2c-'+str(device)+'-'+str(pin)+'.rrd'
+      filename = '/home/pi/bin/pi-van-mon/data/adci2c-'+str(device)+'-'+str(pin)+'.rrd'
 
 
-          if( not os.path.exists( filename ) ):
-            print ( os.path.exists( filename ))
-            os.system('/usr/bin/rrdtool create '+filename+' --step 60 --start now DS:data:GAUGE:120:U:U RRA:AVERAGE:0.5:1:10080 RRA:AVERAGE:0.5:5:51840 RRA:AVERAGE:0.5:60:4380')
+      if( not os.path.exists( filename ) ):
+        print ( os.path.exists( filename ))
+        os.system('/usr/bin/rrdtool create '+filename+' --step 60 --start now DS:data:GAUGE:120:U:U RRA:AVERAGE:0.5:1:10080 RRA:AVERAGE:0.5:5:51840 RRA:AVERAGE:0.5:60:4380')
 
-          os.system('/usr/bin/rrdtool update '+filename+" "+str(t)+':'+data)
+      os.system('/usr/bin/rrdtool update '+filename+" "+str(t)+':'+data)
 
     #except: # Device probably not plugged in :(
     #    pass
