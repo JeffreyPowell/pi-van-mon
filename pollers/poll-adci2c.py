83f6bafed13158a9fca532164142ad8abc519885
Jeff <jffrypwll@googlemail.com>
2017-08-01 20:28:30 +0100

    .

--- pollers/poll-adci2c.py
+++ pollers/poll-adci2c.py
@@ -33,18 +33,18 @@ for device in config['devices']['adci2c']:
           factor = float(config['devices']['adci2c'][device]['pins'][pin]['factor'])
           offset = float(config['devices']['adci2c'][device]['pins'][pin]['offset'])
 
-          #print( factor, offset )
+          print( factor, offset )
 
           rawdata = adc.readVoltage( pin )
           data = str( ( rawdata + offset ) * factor )
 
-          #print( device, pin, rawdata, data )
+          print( device, pin, rawdata, data )
 
           filename = '/home/pi/bin/pi-van-mon/data/adci2c-'+str(device)+'-'+str(pin)+'.rrd'
 
 
           if( not os.path.exists( filename ) ):
-            #print ( os.path.exists( filename ))
+            print ( os.path.exists( filename ))
             os.system('/usr/bin/rrdtool create '+filename+' --step 60 --start now DS:data:GAUGE:120:U:U RRA:AVERAGE:0.5:1:10080 RRA:AVERAGE:0.5:5:51840 RRA:AVERAGE:0.5:60:4380')
 
           os.system('/usr/bin/rrdtool update '+filename+" "+str(t)+':'+data)
