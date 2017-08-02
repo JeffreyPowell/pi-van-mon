f56f5a6ac92ca6b4d3ff2ec7a089a9777c56f6e9
Jeff <jffrypwll@googlemail.com>
2017-08-02 23:56:58 +0100

    .

--- email/email.sh
+++ email/email.sh
@@ -16,7 +16,7 @@ vertaxislabelpt=6
 allaxisvaluept=6
 legendpt=6
 
-/usr/bin/rrdtool graph /home/pi/bin/pi-van-mon/www/images/e-$datafile$start.png \
+result=/usr/bin/rrdtool graph /home/pi/bin/pi-van-mon/www/images/e-$datafile$start.png \
 --start $start \
 --vertical-label "$dataunit" \
 --height=$height \
