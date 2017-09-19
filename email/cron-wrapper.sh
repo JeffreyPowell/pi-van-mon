e7c6356aee1326430e1cb9dd1eb072805cd68568
Jeff <jffrypwll@googlemail.com>
2017-09-19 23:37:40 +0100

    .

--- email/cron-wrapper.sh
+++ email/cron-wrapper.sh
@@ -2,4 +2,4 @@
 /bin/bash /home/pi/bin/pi-van-mon/email/email.sh adci2c-2-1 > /dev/null
 /bin/bash /home/pi/bin/pi-van-mon/email/email.sh adci2c-2-2 > /dev/null
 #/bin/bash /home/pi/bin/pi-van-mon/email/email.sh adci2c-2-3 > /dev/null
-/bin/bash /home/pi/bin/pi-van-mon/email/email.sh adci2c-2-4 > /dev/null
+#/bin/bash /home/pi/bin/pi-van-mon/email/email.sh adci2c-2-4 > /dev/null
