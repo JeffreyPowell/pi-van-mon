e8c80ecb2f9c7088c3746040128af123e56e84c7
Jeff <Jffrypwll@googlemail.com>
2017-10-23 20:36:25 +0100

    Update cron-wrapper.sh

--- email/cron-wrapper.sh
+++ email/cron-wrapper.sh
@@ -1,5 +1,5 @@
 
 /bin/bash /home/pi/bin/pi-van-mon/email/email.sh adci2c-2-1 > /dev/null
 /bin/bash /home/pi/bin/pi-van-mon/email/email.sh adci2c-2-2 > /dev/null
-#/bin/bash /home/pi/bin/pi-van-mon/email/email.sh adci2c-2-3 > /dev/null
-#/bin/bash /home/pi/bin/pi-van-mon/email/email.sh adci2c-2-4 > /dev/null
+/bin/bash /home/pi/bin/pi-van-mon/email/email.sh adci2c-2-3 > /dev/null
+/bin/bash /home/pi/bin/pi-van-mon/email/email.sh adci2c-2-4 > /dev/null
